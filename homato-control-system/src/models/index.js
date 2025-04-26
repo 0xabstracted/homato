b16232e1897bd36cafef45759ee98107b152a122
Raghava  Reddy <raghavareddy@ip-192-168-230-87.ap-south-1.compute.internal>
2025-04-26 17:20:02 +0530

    Activity file name change

--- homato-control-system/src/models/index.js
+++ homato-control-system/src/models/index.js
@@ -2,7 +2,7 @@
 const User = require('./user.js'); 
 const Device = require('./device.js');
 const Switch = require('./Switch.js');
-const Activity = require('./Activity.js');
+const Activity = require('./activity.js');
 const UserDevice = require('./UserDevice.js');
 
 module.exports = {
